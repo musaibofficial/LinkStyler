534206bcf8e0a9ef6a5861609cf0976f8fbbae00
musaib821 <92442385+musaib821@users.noreply.github.com>
2023-02-22 09:21:30 +0530

    Update main.py
    Bgg

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import requests 
+#import requests 
 import telebot 
-url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQDGJFA3YSUBaJkF5yo0P_nzleEOqFGUYqgw&usqp=CAU'
-r=requests.get(url, allow_redirects=True)
-open('facebook.png', 'wb').write(r.content)
+#url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQDGJFA3YSUBaJkF5yo0P_nzleEOqFGUYqgw&usqp=CAU'
+#r=requests.get(url, allow_redirects=True)
+"""open('facebook.png', 'wb').write(r.content)
 response = requests.post(
     'https://api.remove.bg/v1.0/removebg',
     files={'image_file': open('facebook.png', 'rb')},
@@ -13,12 +13,12 @@ if response.status_code == requests.codes.ok:
     with open('no-bg.png', 'wb') as out:
         out.write(response.content)
         f=response.content 
-else:
-    print("Error:", response.status_code, response.text) 
+else:"""
+  #  print("Error:", response.status_code, response.text) 
 bot=telebot.TeleBot('5989308938:AAFrPoIHzm3KlIlPCsIIVQEu0uGd9RtmDb4')
 @bot.message_handler(commands=['start'])
 def cry(m):
   bot.reply_to(m, 'Hi Sending')
-  bot.send_photo(m.chat.id,f)
+ # bot.send_photo(m.chat.id,f)
 bot.infinity_polling ()
 
