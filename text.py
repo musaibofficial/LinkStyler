c6812518241933b1731641be6d9068f122c54ba4
musaib821 <92442385+musaib821@users.noreply.github.com>
2023-02-23 13:05:18 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,7 @@
+intro2="Hey There! 👋 Welcome To The Link Styler Bot 🤖. My Name Is [Insert Bot Name Here], And I'm Here To Help You Make Your Links Pop 💥. Want To Give Your Channel A Fresh Look With Some Customized Links And Buttons?" 
+
+setting="This Bot Is Easy To Understand😀 \n •) Set The Title For Your Message\n •Change Style Using ⚙️ \n •) Your Data Is Not Stored Anywhere So You Have To Set These Values Everytime To Use Me ♻️"
+
+row_num="•Defaut is Set To 2 Buttons In Row \n •) Currently We Support Only 2 Buttons In Row,It Will Be Increased In Next Update \n •) HTML links Will Be Available In Next Update \n Join Channel To Receive New Updates 🌟"
+
+links_txt="💡 You Can Send Any Photo With Links In Caption Or Any Text With Links \n Use /help To Access Help Section"
